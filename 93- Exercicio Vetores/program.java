bfb92d36d6609751928d63d82bc416c90237f545
Felipe Schmitz <aryastarklinda@outlook.com>
2023-10-04 17:41:37 -0300

    exercicio 93

---
+++ 93- Exercicio Vetores/program.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Rent r = new Rent("Maria Green", "maria@gmail.com");
+        // System.out.println(r);
+
+        Rent[] vect =  new Rent[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            System.out.println("Rent #" + (i + 1) + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int roomNumber = sc.nextInt();
+
+            vect[roomNumber] = new Rent(name, email);
+        }
+
+        
+        // for tests
+        // vect[5] = new Rent("Maria Green", "maria@gmail.com");
+        // vect[1] = new Rent("Marco Antonio", "marco@gmail.com");
+        // vect[8] = new Rent("Alex Brown", "alex@gmail.com");
+
+        
+        System.out.println();
+        System.out.println("Busy rooms: ");
+        for( int i = 0; i < vect.length; i++ ){
+            if( vect[i] != null ){
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
