6791e3a2ae0eddbd667a4d771964f7af796afe2a
Felipe Schmitz <aryastarklinda@outlook.com>
2023-11-18 09:32:29 -0300

    exercicio dois

---
+++ Interfaces dois/application/MainInterfaceExercicioDois.java
@@ -0,0 +1,44 @@
+package application;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MainInterfaceExercicioDois {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o número de parcelas: ");
+        int numberInstallments = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(contract, numberInstallments);
+
+        System.out.println("Parcelas:");
+
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
