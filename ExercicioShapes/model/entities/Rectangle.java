f4dd863b9fbcdf7dc4a5405475941e57ff44bc73
Felipe Schmitz <aryastarklinda@outlook.com>
2023-11-21 15:13:40 -0300

    Exercicio com versão sem inteface

---
+++ ExercicioShapes/model/entities/Rectangle.java
@@ -0,0 +1,36 @@
+package model.entities;
+
+import model.enums.Color;
+
+public class Rectangle extends Shape{
+
+    private double height;
+    private double width;
+
+    public Rectangle(Color color, double height, double width) {
+        super(color);
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
